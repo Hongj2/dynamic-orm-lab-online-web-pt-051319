08c6d1a057094305c09857ecb273e584ff3f8fbe
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-28 05:06:00 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,20 +5,22 @@ class InteractiveRecord
   def self.table_name 
     self.to_s.downcase.pluralize 
   end
-  # to create an abstract method that could be applied to any class and grab self (the class) name and change it into a string, downcase it and pluralize it because the table hold all the instance of the #class object  eg. class Dog ==> dogs
+  
   
   def self.column_names 
     DB[:conn].results_as_hash = true
+    
     sql = "PRAGMA table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
+    
     column_names = []
     
-    table_info.each do |column| 
-      column_names << column["name"]
+    table_info.each do |row| 
+      column_names << row["name"]
     end
     column_names.compact
   end
-  # the purpose of this method is draw out the hash of all of row and then iterate through it row and declare the name of that row and saving it into the empty array that is equal into the variable #'column_names', the results is still in an form of a key value pair and with the use of compact is to remove that relationship and to remove any residual nils and remaining elements are the attributes 
+ 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
